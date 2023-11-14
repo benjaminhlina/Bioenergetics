1de40cbc8b4d81877c6081107a856ee26a8f949f
benjaminhlina <benjamin.hlina@gmail.com>
2023-11-14 15:20:01 -0500

    Updated Figure 5. MMR, RMR, SMR, with AS, and SoF plot, to start in Jan not May

--- R/Plot Publishing Scripts/metabolism_scope_plots..R
+++ R/Plot Publishing Scripts/metabolism_scope_plots..R
@@ -10,20 +10,18 @@ library(readr)
 
 # --- bring in plots ---- 
 a <- read_rds(here("Plot Objects", 
-                    "mmr_rmr_smr.rds"))
+                    "mmr_rmr_smr_jan.rds"))
 
 
 b <- read_rds(here("Plot Objects", 
-                  "Scope_of_activity_and_AS_gamm_plot.rds"))
-c <- read_rds(here("Plot Objects", 
-                    "mmr_rmr_smr.rds"))
+                  "Scope_of_activity_and_AS_gamm_plot_jan.rds"))
+c <- a
 
-
-d <- read_rds(here("Plot Objects", 
-                  "Scope_of_activity_and_AS_gamm_plot.rds"))
-
+d <- b
 # glimpse(a)
 
+
+a$layers[[1]] <- NULL
 a$layers[[1]] <- NULL
 a$layers[[1]] <- NULL
 a$layers[[1]] <- NULL
@@ -46,7 +44,7 @@ p <- a / b +
 p
 ggsave(plot = p, filename = here("plots",
                                   "Combined metabolic rates",
-                                  "metabolism_and_scope_plot_wo_grey.png"), 
+                                  "metabolism_and_scope_plot_wo_grey_CORRECT_pt_jan.png"), 
        width = 11,
        height = 7 * 2)
 p1 <- c / d + 
@@ -58,7 +56,42 @@ p1 <- c / d +
 p1
 ggsave(plot = p1, filename = here("plots",
                                   "Combined metabolic rates",
-                                  "metabolism_and_scope_plot.png"), 
+                                  "metabolism_and_scope_plot_CORRECT_pt_jan.png"), 
        width = 11,
        height = 7 * 2)
 
+a$layers[[1]] <- NULL
+b$layers[[1]] <- NULL
+
+p2 <- a / b + 
+  plot_annotation(
+    tag_levels = "a",
+    tag_suffix = ")"
+  )
+
+# p2
+ggsave(plot = p2, filename = here("plots",
+                                 "Combined metabolic rates",
+                                 "metabolism_and_scope_plot_wo_grey_CORRECT_jan.png"), 
+       width = 11,
+       height = 7 * 2)
+
+# c$layers[[4]] <- NULL
+# c$layers[[4]] <- NULL
+# c$layers[[4]] <- NULL
+# d$layers[[3]] <- NULL
+# d$layers[[3]] <- NULL
+
+
+p3 <- c / d + 
+  plot_annotation(
+    tag_levels = "a",
+    tag_suffix = ")"
+  )
+
+p3
+ggsave(plot = p3, filename = here("plots",
+                                  "Combined metabolic rates",
+                                  "metabolism_and_scope_plot_CORRECT_jan.png"), 
+       width = 11,
+       height = 7 * 2)
