5c15e537b441c5c8c6338a43cba58abd35d3c9f7
benjaminhlina <benjamin.hlina@gmail.com>
2023-11-14 15:12:01 -0500

    Updated MMR, RMR and SMR plots to be DOY and not DOY_ID

--- R/Aerobic Scope/mmr_rmr_smr.R
+++ R/Aerobic Scope/mmr_rmr_smr.R
@@ -37,7 +37,7 @@ as_gamm <- read_rds(here("Saved Data",
 
 
 mmr_gamm <- read_rds(here("Saved Data",
-                     "mmr_gamm_predict.rds"))
+                          "mmr_gamm_predict.rds"))
 
 smr_gamm <- read_rds(here("Saved Data", 
                           "smr_gamma_predict.rds")) %>% 
@@ -50,53 +50,120 @@ glimpse(as_gamm)
 glimpse(rmr_gamm)
 glimpse(smr_gamm)
 
-dat_list <- list(as = as_gamm, mmr = mmr_gamm,
+
+mean_mmr <- read_rds(here::here("Model Objects", 
+                                "mean_mmr.rds")) %>% 
+  rename(
+    mmr = mean_mmr
+  ) %>% 
+  pivot_longer(
+    cols = -doy,
+    # cols = -doy, 
+               names_to = "type", 
+               values_to = "est")
+
+mean_rmr <- read_rds(here::here("Model Objects", 
+                                "mean_rmr.rds")) %>% 
+  pivot_longer(
+    # cols = -doy, 
+    cols = -doy,
+               names_to = "type", 
+               values_to = "est")
+mean_smr <- read_rds(here::here("Model Objects", 
+                                "mean_smr.rds")) %>% 
+  rename(
+    smr = mean_smr) %>% 
+  dplyr::select(-mean_temp) %>% 
+  pivot_longer(
+    # cols = -doy, 
+    cols = -doy,
+               names_to = "type", 
+               values_to = "est")
+glimpse(mean_mmr)
+glimpse(mean_rmr)
+glimpse(mean_smr)
+
+met_pnts <- bind_rows(
+  mean_mmr, mean_rmr, mean_smr
+)
+
+
+dat_list <- list(mmr = mmr_gamm,
                  rmr = rmr_gamm, smr = smr_gamm)
 
 slim_dat <- dat_list %>% 
   map(., ~ .x %>% 
-        dplyr::select(doy_id, fit, lower, upper)) %>% 
+        dplyr::select(doy, doy, fit, lower, upper)) %>% 
   bind_rows(.id = "type")
 slim_dat
 
 
 
-# create month labels 
-smr_gamm %>% 
-  filter(doy_id %in% seq(25, 350, 65)) %>% 
-  group_by(year, month_abb) %>% 
+# create month lablel ----- 
+
+month_doy <- mmr_gamm %>% 
+  group_by(month_abb) %>% 
+  summarise(first = first(doy),
+            last = last(doy)) %>% 
+  ungroup() %>% 
+  # mutate(
+  #   month_abb = forcats::fct_relevel(month_abb, "Jan", 
+  #                                    "Feb", "Mar", "Apr", "May", "Jun",
+  #                                    "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
+  # ) %>% 
+  arrange(month_abb) %>%   
+  .$first
+mmr_gamm %>% 
+  filter(doy %in% month_doy) %>%
+  group_by(month_abb) %>% 
   summarise() %>% 
+  # mutate(
+  #   month_abb = forcats::fct_relevel(month_abb, "Jan", 
+  #                                    "Feb", "Mar", "Apr", "May", "Jun",
+  #                                    "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
+  # ) %>% 
+  arrange(month_abb) %>% 
   .$month_abb -> month_label 
 month_label
-
 # plotting prep -------
 
 # figure out where your shading for summer and winter goes 
+predicts %>% 
+  group_by(season) %>% 
+  summarise(first = first(doy),
+            last = last(doy)) %>% 
+  ungroup()
 
 rect_summer <- tibble(
   season = "Summer",
-  xmin = 32,
-  xmax = 123,
+  xmin = 152,
+  xmax = 244,
   ymin = -Inf,
   ymax = Inf
 )
 
 rect_winter <- tibble(
   season = "Winter",
-  xmin = 215,
-  xmax = 305,
+  xmin = 1,
+  xmax = 60,
+  ymin = -Inf,
+  ymax = Inf
+)
+rect_winter_dec <- tibble(
+  season = "Winter",
+  xmin = 335,
+  xmax = 365,
   ymin = -Inf,
   ymax = Inf
 )
 
 
-
 # ---- filter and remove aerobic scope ---- 
 
-slim_dat_a <- slim_dat %>% 
-  filter(
-    type != "as"
-  )
+# slim_dat_a <- slim_dat %>% 
+#   filter(
+#     type != "as"
+#   )
 
 
 # ------ plot -----
@@ -115,22 +182,35 @@ ggplot() +
             fill ="grey80",
             alpha = 0.75,
             inherit.aes = FALSE) +
+  geom_rect(data = rect_winter_dec, aes(xmin = xmin,
+                                    xmax = xmax,
+                                    ymin = ymin,
+                                    ymax = ymax),
+            fill ="grey80",
+            alpha = 0.75,
+            inherit.aes = FALSE) +
   geom_text(
-    aes(x = xmin + 30, y = 198.25, label = season),
+    aes(x = xmin + 33, y = 198.25, label = season),
     data = rect_summer,
     size = 5, vjust = 0, hjust = 0, check_overlap = TRUE) +
   geom_text(
-    aes(x = xmin + 32, y = 198.25, label = season),
+    aes(x = xmin + 17.5, y = 198.25, label = season),
     data = rect_winter,
     size = 5, vjust = 0, hjust = 0, check_overlap = TRUE) + 
-  geom_line(data = slim_dat_a, 
-            aes(x = doy_id, y = fit, 
+  geom_text(
+    aes(x = xmin + 5, y = 198.25, label = season),
+    data = rect_winter_dec,
+    size = 5, vjust = 0, hjust = 0, check_overlap = TRUE) +
+  geom_point(data = met_pnts, aes(x = doy, y = est, colour = type), 
+             alpha = 0.25) +
+  geom_line(data = slim_dat, 
+            aes(x = doy, y = fit, 
                 colour = type
             ), linewidth = 1) +
-  geom_ribbon(data = slim_dat_a, 
+  geom_ribbon(data = slim_dat, 
               aes(ymin = lower,
                   ymax = upper,
-                  x = doy_id, y = fit,
+                  x = doy, y = fit,
                   fill = type, 
               ), alpha = 0.25) +
   scale_fill_viridis_d(name = "",
@@ -143,7 +223,7 @@ ggplot() +
                          labels = c("Maximum \nMetabolism (MMR)", 
                                     "Active \nMetabolism (RMR)",
                                     "Standard \nMetabolism (SMR)")) +
-  scale_x_continuous(breaks = seq(25, 350, 65), 
+  scale_x_continuous(breaks = month_doy, 
                      label = month_label) +
   scale_y_continuous(breaks = seq(0, 200, 20)) +
   coord_cartesian(ylim = c(20, 200)) +
@@ -151,19 +231,19 @@ ggplot() +
   theme(panel.grid = element_blank(),
         strip.text = element_blank(),
         axis.text = element_text(colour = "black"),
-        legend.position = c(0.9, 0.92),
+        legend.position = c(0.30, 0.92),
         legend.spacing.y = unit(0.75, 'cm'),
         legend.title = element_text(hjust = 0.5),
         legend.text = element_text(hjust = 0.5),
         legend.background = element_blank()) +
- guides(
-   fill = guide_legend(byrow = TRUE),
-   colour = guide_legend(byrow = TRUE),
-   ) + 
+  guides(
+    fill = guide_legend(byrow = TRUE),
+    colour = guide_legend(byrow = TRUE),
+  ) + 
   labs(x = "Date", 
        y = expression(paste("Metabolic Rate (mg", 
                             O[2]," ", kg^-1, " ", h^-1, ")"))) -> p 
 
-# p
+p
 write_rds(p, here("Plot Objects", 
-                  "mmr_rmr_smr.rds"))
+                  "mmr_rmr_smr_jan.rds"))
