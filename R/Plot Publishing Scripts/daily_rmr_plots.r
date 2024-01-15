a8b1e11583235756b124f3a1e3d22cf72af700d0
benjamin.hlina <benjamin.hlina@gmail.com>
2024-01-15 09:44:27 -0500

    Updated plot publishing script

--- R/Plot Publishing Scripts/daily_rmr_plots.r
+++ R/Plot Publishing Scripts/daily_rmr_plots.r
@@ -71,10 +71,15 @@ vi_2 <- vi +
 p3 <- gam / vi_2 + 
   plot_annotation(tag_levels = "a", 
                   tag_suffix = ")")
-# p3
+p3
 
 ggsave(plot = p3, filename = here("plots",
                                   "Combined GAMM and Violin", 
                                   "gamm_violin_rmr_doy_jan.png"), 
+       width = 11,
+       height = 7 * 2)
+ggsave(plot = p3, filename = here("plots",
+                                  "Combined GAMM and Violin", 
+                                  "gamm_violin_rmr_doy_jan.pdf"), 
        width = 11,
        height = 7 * 2)
